8b430693a926ebfce972c6dd646e03cb6c6fda26
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-03-17 15:46:48 +0200

    Add 2.5.2 exercises solutions

--- examples/script.py
+++ examples/script.py
@@ -8,3 +8,12 @@ pere
 banane
 """
 print(multiline_str)
+
+distance = 5.7212
+print("Iulia walked 5.12km.")
+print(name, "walked", round(distance, 2), "km.")
+message = name + " walked " + str(round(distance, 2)) + "km."
+print(message)
+
+message = f"{name} walked {distance:.2f}km."
+print(message)
