daded955b0fb3c438fdd2a07005d961f07cdd61c
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-03-19 11:05:52 +0200

    Add comments to explain shadowing

--- examples/scope.py
+++ examples/scope.py
@@ -3,9 +3,12 @@ x = 0  # global variable
 
 def func(a):
     b = "b"  # local variable
+    # shadowing of global variable (it is recommended to avoid shadowing names)
+    # x = 100
     print("inside func:", a, b, x)
 
 
+# sum = 0  # shadowing of a built-in name
 func(25)
 func(42)
 print("outside func:", x)
