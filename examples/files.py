b9002b8d3835a8f0a1e1412d1829698739bc387a
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-03-20 11:56:43 +0200

    Add files and dictionaries exercises solutions

--- examples/files.py
+++ examples/files.py
@@ -1,1 +1,10 @@
 my_path = r"D:\Ceva\Altceva\nanana"  # for Windows paths, use raw strings
+
+with open("imports.py", "r") as f:
+    print("inside `with` block, file is closed:", f.closed)
+    for line in f:
+        if line.startswith("#"):
+            print(line.strip())
+
+print("after `with` block, file is closed:", f.closed)
+
